a0ead6b2c6d6e15cfe66f67675206c7111677900
Bodzisz <kacpiw01pol@gmail.com>
2021-10-19 01:52:33 +0200

    Add main application

---
+++ backend/src/main/java/io/github/bodzisz/BackendRecruitmentTaskApplication.java
@@ -0,0 +1,14 @@
+package io.github.bodzisz;
+
+import io.github.bodzisz.repository.TablesInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRecruitmentTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRecruitmentTaskApplication.class, args);
+	}
+
+}
